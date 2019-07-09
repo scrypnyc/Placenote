dd766f6cfb0ae4025a07f5aca68b27679e844a11
Alexander <alexanderlookua@gmail.com>
2019-07-09 20:36:00 +0300

    First commit
    Add tableView

---
+++ TableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewController.swift
+//  Placenote
+//
+//  Created by Alexander Skrypnyk on 7/9/19.
+//  Copyright © 2019 Alexander Skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "GovnoSobach'e mat' tvou"
+        cell?.textLabel?.textColor = .white
+        return cell!
+    }
+}

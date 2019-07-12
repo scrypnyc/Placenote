d994e213e7bab23c691e38a413ac0889f45bde40
Alexander <alexanderlookua@gmail.com>
2019-07-12 22:38:10 +0300

    Update TableVC

--- TableViewController.swift
+++ TableViewController.swift
@@ -9,19 +9,23 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    let placeNames = ["Milk Bar", "Come and Stay", "One Love Coffee", "Любимый Дядя", "Косатка"]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
+    // MARK: Table View
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return placeNames.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = "GovnoSobach'e mat' tvou"
+        cell?.textLabel?.text = placeNames[indexPath.row]
         cell?.textLabel?.textColor = .white
         return cell!
     }
